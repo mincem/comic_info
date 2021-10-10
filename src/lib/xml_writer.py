2040ca5ea4b721cb084b33462a8d130e0a8f7bb8
Matías Incem <matiasincem@gmail.com>
2021-10-10 19:18:20 -0300

    Fix xml file path

--- src/lib/xml_writer.py
+++ src/lib/xml_writer.py
@@ -4,7 +4,7 @@ from xml.etree import cElementTree as elementTree
 
 
 def write_xml(xml_element, directory_path):
-    file_path = os.path.join(directory_path, "../../ComicInfo.xml")
+    file_path = os.path.join(directory_path, "ComicInfo.xml")
     print(f"Saving to {file_path}")
     parsed_xml = minidom.parseString(elementTree.tostring(xml_element, encoding="utf-8"))
     pretty_xml = parsed_xml.toprettyxml(indent="  ", encoding="utf-8")
