1bbb7d85c217617ef10503d37c5fad28b79e0546
Matías Incem <matiasincem@gmail.com>
2021-10-18 16:13:49 -0300

    Add option to hardcode starting dirs

--- src/main.py
+++ src/main.py
@@ -3,8 +3,14 @@ from argparse import ArgumentParser
 
 from lib.command import Command
 
+STARTING_DIRS = []
+
 if __name__ == '__main__':
     parser = ArgumentParser(description='Add ComicInfo file to comic book Zip files.')
-    parser.add_argument('startingDirectory', help='Path to the starting directory.')
+    parser.add_argument('startingDirectory', nargs="?", help='Path to the starting directory.')
     arguments = parser.parse_args(sys.argv[1:])
-    Command(starting_dir=arguments.startingDirectory).execute()
+    if arguments.startingDirectory is None:
+        for starting_dir in STARTING_DIRS:
+            Command(starting_dir=starting_dir).execute()
+    else:
+        Command(starting_dir=arguments.startingDirectory).execute()
